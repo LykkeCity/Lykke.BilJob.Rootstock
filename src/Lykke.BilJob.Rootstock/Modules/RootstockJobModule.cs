9a15c7123cb1b559ef715f3803972151c76a35b0
Yury Glushkov <mail@glushkov.us>
2018-12-12 20:10:01 +0300

    Project structure fixed

--- src/Lykke.BilJob.Rootstock/Modules/RootstockJobModule.cs
+++ src/Lykke.BilJob.Rootstock/Modules/RootstockJobModule.cs
@@ -1,18 +1,19 @@
 using Autofac;
 using JetBrains.Annotations;
 using Lykke.BilJob.Rootstock.Settings;
+using Lykke.Quintessence.DependencyInjection;
 using Lykke.Quintessence.Settings;
-using Lykke.Quintessence.Utils;
+using Lykke.SettingsReader;
 
 namespace Lykke.BilJob.Rootstock.Modules
 {
     [UsedImplicitly]
     public class RootstockJobModule : Module
     {
-        private readonly AppSettings<RootstockJobSettings> _appSettings;
+        private readonly IReloadingManager<AppSettings<RootstockJobSettings>> _appSettings;
 
         public RootstockJobModule(
-            AppSettings<RootstockJobSettings> appSettings)
+            IReloadingManager<AppSettings<RootstockJobSettings>> appSettings)
         {
             _appSettings = appSettings;
         }
@@ -21,7 +22,13 @@ namespace Lykke.BilJob.Rootstock.Modules
             ContainerBuilder builder)
         {
             builder
-                .RegisterRootstock(_appSettings.Job.IsMainNet);
+                .UseRootstock
+                (
+                    _appSettings.ConnectionString(x => x.Job.Db.DataConnString),
+                    _appSettings.Nested(x => x.Job.ConfirmationLevel),
+                    _appSettings.ConnectionString(x => x.Job.GasPriceRange),
+                    _appSettings.CurrentValue.Job.IsMainNet
+                );
         }
     }
 }
