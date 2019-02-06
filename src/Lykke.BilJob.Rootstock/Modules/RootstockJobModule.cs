1c92df0a3cef6efbc8172cdf116804a4e566dc45
Unknown <mail@glushkov.us>
2019-02-06 10:04:58 +0300

    BIT-128: RSK Job Implemented

---
+++ src/Lykke.BilJob.Rootstock/Modules/RootstockJobModule.cs
@@ -0,0 +1,36 @@
+using Autofac;
+using JetBrains.Annotations;
+using Lykke.BilJob.Rootstock.Settings;
+using Lykke.Quintessence.Core.Telemetry.DependencyInjection;
+using Lykke.Quintessence.DependencyInjection;
+using Lykke.Quintessence.Settings;
+using Lykke.SettingsReader;
+
+namespace Lykke.BilJob.Rootstock.Modules
+{
+    [UsedImplicitly]
+    public class RootstockJobModule : Module
+    {
+        private readonly IReloadingManager<AppSettings<RootstockJobSettings>> _appSettings;
+
+        public RootstockJobModule(
+            IReloadingManager<AppSettings<RootstockJobSettings>> appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        protected override void Load(
+            ContainerBuilder builder)
+        {
+            builder
+                .UseAITelemetryConsumer()
+                .UseRootstock
+                (
+                    _appSettings.ConnectionString(x => x.Job.Db.DataConnString),
+                    _appSettings.Nested(x => x.Job.ConfirmationLevel),
+                    _appSettings.ConnectionString(x => x.Job.GasPriceRange),
+                    _appSettings.CurrentValue.Job.IsMainNet
+                );
+        }
+    }
+}
